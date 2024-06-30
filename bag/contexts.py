08ad3b03ac3120a53eef2ed88dac2615b912f1e0
Dan Matthews <dan.matthews1987@gmail.com>
2024-06-30 07:08:36 +0000

    UPDATED all pages to refactor code

--- bag/contexts.py
+++ bag/contexts.py
@@ -6,7 +6,7 @@ from products.models import Product
 
 def bag_contents(request):
 
-    bag_items = []
+    basket_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
@@ -16,7 +16,7 @@ def bag_contents(request):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
             product_count += item_data
-            bag_items.append({
+            basket_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
@@ -26,7 +26,7 @@ def bag_contents(request):
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
-                bag_items.append({
+                basket_items.append({
                     'item_id': item_id,
                     'quantity': quantity,
                     'product': product,
@@ -43,7 +43,7 @@ def bag_contents(request):
     grand_total = delivery + total
 
     context = {
-        'bag_items': bag_items,
+        'basket_items': basket_items,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
